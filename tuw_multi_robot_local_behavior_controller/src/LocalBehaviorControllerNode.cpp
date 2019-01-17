29de0c7ed4fce38a6416c9f0d8344617139e476b
Theo Steger <theo.steger@ifollow.fr>
2019-01-17 18:42:08 +0100

    Correction of pose callback in local behavior

--- tuw_multi_robot_local_behavior_controller/src/LocalBehaviorControllerNode.cpp
+++ tuw_multi_robot_local_behavior_controller/src/LocalBehaviorControllerNode.cpp
@@ -162,8 +162,10 @@ void LocalBehaviorControllerNode::subPoseCb ( const geometry_msgs::PoseWithCovar
       ros::Time now = ros::Time::now();
       if(tf_listener_.waitForTransform(_pose->header.frame_id, frame_id_, now, ros::Duration(1.0)))
         tf_listener_.transformPose(frame_id_, pose_odom_frame, pose_world_frame);
-      else
+      else {
         ROS_ERROR("Local behavior: transform between %s and %s is not available",_pose->header.frame_id.c_str(),frame_id_.c_str());
+        return;
+      } 
     } catch (tf::TransformException ex){
       ROS_ERROR("%s",ex.what());
       ros::Duration(1.0).sleep();
